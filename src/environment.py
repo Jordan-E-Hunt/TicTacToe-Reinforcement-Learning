a2f1a9f707b2ec5e0464d9fb2a0e78983fe1413c
Jordan Hunt <jordan0110hunt@gmail.com>
2026-02-05 17:47:58 -0600

    update environment for symmetry

--- src/environment.py
+++ src/environment.py
@@ -2,42 +2,45 @@ import numpy as np
 
 class TicTacToe:
   def __init__(self):
-    self.board = np.zeros((3,3)) #3x3 Board
+    self.size = 3 #3x3 Board
+    self.board = np.zeros((self.size,self.size)) 
     self.players = ["X", "O"]
     self.current = self.players[0]
     self.winner = None
     self.game_over = False
     self.check_winner()
 
- def reset(self):
+  def reset(self):
     self.__init__()
 
- def avail_moves(self):
+  def avail_moves(self):
     moves = [] #Will contain list of moves available
-    for i in range(3):
-      for j in range(3):
+    for i in range(self.size):
+      for j in range(self.size):
         if self.board[i][j] == 0:
           moves.append((i,j))
     return moves
 
- def get_state(self):
+  def get_state(self):
     #Add Perspective Variation (Bring Accuracy Above 75%)
     p_idx = self.players.index(self.current) + 1 #1 or 2
 
     #Create a copy where current is 1 and 'other' is -1
-    norm_board = np.zeros((3,3))
-    for i in range(3):
-        for j in range(3):
+    norm_board = np.zeros((self.size,self.size))
+    for i in range(self.size):
+        for j in range(self.size):
             if self.board[i][j] == 0:
                 norm_board[i][j] = 0
             elif self.board[i][j] == p_idx:
                 norm_board[i][j] = 1  # Me
             else:
                 norm_board[i][j] = -1 # Opponent
+    
+    canonical = self.canonical_board(norm_board)
 
     return tuple(norm_board.flatten())
 
- def make_move(self, move):
+  def make_move(self, move):
     reward = -0.1
     if self.board[move[0]][move[1]] != 0:
       return self.get_state(), -1.0, True, False
@@ -59,20 +62,20 @@ class TicTacToe:
 
     return self.get_state(), reward, self.game_over, False
 
- def switch_player(self):
+  def switch_player(self):
     if self.current == self.players[0]:
       self.current = self.players[1]
     else:
       self.current = self.players[0]
 
- def check_winner(self):
+  def check_winner(self):
     #Row Wins
-    for i in range(3):
+    for i in range(self.size):
       if self.board[i][0] == self.board[i][1] ==  self.board[i][2] != 0:
         self.winner = self.players[int(self.board[i][1] - 1)]
         self.game_over = True
     #Column Wins
-    for j in range(3):
+    for j in range(self.size):
       if self.board[0][j] == self.board[1][j] == self.board[2][j] != 0:
         self.winner = self.players[int(self.board[1][j] - 1)]
         self.game_over = True
@@ -83,16 +86,39 @@ class TicTacToe:
       self.game_over = True
 
     #Draw
-    if np.all(self.board != 0):
+    if np.all(self.board != 0) and self.game_over == False:
       self.winner = "Draw"
       self.game_over = True
 
- def print_board(self):
+  def print_board(self):
     print("--------------")
-    for i in range(3):
+    for i in range(self.size):
       print("|", end= " ")
-      for j in range(3):
+      for j in range(self.size):
          print(self.players[int(self.board[i][j] - 1)]
           if self.board[i][j] != 0 else " ", end=" | ")
       print()
       print("--------------")
+  
+  def canonical_board(self, board):
+    "Return the lex-smallest tuple among all 8 symmetries."
+    candidates = []
+    
+    # Original
+    candidates.append(tuple(board.flatten()))
+    
+    # Rotations
+    rot90 = np.rot90(board)
+    candidates.append(tuple(rot90.flatten()))
+    rot180 = np.rot90(rot90)
+    candidates.append(tuple(rot180.flatten()))
+    rot270 = np.rot90(rot180)
+    candidates.append(tuple(rot270.flatten()))
+    
+    if self.size > 3:# Reflections (Not Needed for 3x3 version or smaller, but for scale)
+      candidates.append(tuple(np.fliplr(board).flatten()))  # Horizontal
+      candidates.append(tuple(np.flipud(board).flatten()))  # Vertical
+      candidates.append(tuple(np.transpose(board).flatten()))  # Main diagonal
+      candidates.append(tuple(np.fliplr(np.flipud(board)).flatten()))  # Anti-diagonal
+    
+    return min(candidates)  # Lex smallest
